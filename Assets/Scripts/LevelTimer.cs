dd45c4d71d97d83751e198afc248cbaa27ea6301
Brian Cho <b.cho@queensu.ca>
2022-08-07 15:07:33 -0700

    Level Refactor

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelTimer : MonoBehaviour
+{
+    [SerializeField] TMP_Text levelTimerText;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
